18ac152b34922159c3fe7d6f89b10e08aabfb7c6
mauliktagline <maulik.tagline@gmail.com>
2022-04-29 16:44:45 +0530

    app file changed for the static file read

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.static(`${__dirname}/uploads`));
 
 
